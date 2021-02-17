51d636ab11fd345cc082d5bd52f6ebbb532da1a1
Lorenna Vieira <61518805+lorennav@users.noreply.github.com>
2021-02-17 15:56:59 +0000

    added ft_memccpy.c

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -12,7 +12,21 @@
 
 #include "libft.h"
 
-void *memccpy(void *dst, const void *src, int c, size_t n)
+void	*ft_memccpy(void *dst, const void *src, int c, size_t n)
 {
-    
+	char	*dst_cpy;
+	char	*src_cpy;
+	size_t	index;
+
+	dst_cpy = (char *)dst;
+	src_cpy = (char *)src;
+	index = 0;
+	while (index < n)
+	{
+		dst_cpy[index] = src_cpy[index];
+		if ((unsigned char)src_cpy[index] == (unsigned char)c)
+			return ((char *)dst + index);
+		index++;
+	}
+	return (NULL);
 }
